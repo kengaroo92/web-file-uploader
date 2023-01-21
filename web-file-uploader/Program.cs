628521eb732148f67469af433270d8f28948444e
kengaroo92 <kbjelose1992@gmail.com>
2023-01-21 16:45:27 -0700

    Add project files.

---
+++ web-file-uploader/Program.cs
@@ -0,0 +1,15 @@
+namespace web_file_uploader
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Hello World!");
+
+            app.Run();
+        }
+    }
+}

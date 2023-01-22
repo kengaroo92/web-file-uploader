67041dfa5a15f3a0ff571188fd250e0a4255ef21
kengaroo92 <kbjelose1992@gmail.com>
2023-01-21 19:12:25 -0700

    Further adding configurations to the Program and Startup methods. Attempting to route the Controller to the View. Trying to use Session State, but receiving an error that I'm trying to use Session State but I'm not providing a distributed cache. I'm not even sure if this is needed. Session State is server-side code for handling authentication tokens, shopping carts, etc., not sure why I am getting errors that I need it.

--- web-file-uploader/Program.cs
+++ web-file-uploader/Program.cs
@@ -1,5 +1,3 @@
-using Microsoft.Extensions.FileSystemGlobbing.Internal.Patterns;
-
 namespace web_file_uploader
 {
     public class Program
@@ -13,25 +11,34 @@ namespace web_file_uploader
                     // Specify the startup class for the application.
                     // .NET 6 removed the need for the 'Startup.cs' class. You can use the 'UseStartup' method to configure the middleware pipeline.
                     webBuilder.UseStartup<Startup>();
-                    webBuilder.UseRouting();
-                    webBuilder.UseEndpoints(endpoints =>
-                    {
-                        // Endpoints for routing to the controller.
-                        endpoints.MapControllerRoute(
-                            name: "default",
-                            PatternBuilder: "{controller=Home}/{action=Index}/{id?}");
-                    });
                 }).Build();
-        
+
             await host.RunAsync();
         }
     }
     
     public class Startup
     {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Register required services for the session state.
+            // Session state allows you to store data in a per-user session. Provides a unique session ID.
+            services.AddDistributedMemoryCache();
+            services.AddSession();
+            services.AddControllers();
+        }
+
         public void Configure(IApplicationBuilder app)
         {
-
+            app.UseStaticFiles();
+            app.UseSession();
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
         }
     }
 }

3c5720c081fb7ecdc0ec63697f27206112ed1db9
kengaroo92 <kbjelose1992@gmail.com>
2023-01-21 20:51:50 -0700

    1. Implemented majority of the file uploading functionality that is handled by the client-side. 2. Initialized a new instance of the HttpClient class to handle the file transfer to the external handler. 3. Injected the IWebHostEnvironment to the HomeController. This handles the access to the file root path which is used to determine where the files are going when they come into contact with the file handler. 4. Declared all properties for the files as nullable. ".NET 6 FEATURE"

--- web-file-uploader/Controllers/HomeController.cs
+++ web-file-uploader/Controllers/HomeController.cs
@@ -2,15 +2,69 @@
 {
     public class HomeController : Controller
     {
+        // IWebHostEnvironment is used to access the web root path. This injects it into the controller.
+        // Without this code, we can't access the root of the file path that we need to send to the handler for processing.
+        private readonly IWebHostEnvironment _environment;
+        public HomeController(IWebHostEnvironment environment)
+        {
+            _environment = environment;
+        }
+
         public IActionResult Index()
         {
-            //return Content("Adding this string for testing purposes to see if my Controller is properly routed.");
             return View("Index");
         }
 
-        public IActionResult UploadFiles(IFormFileCollection files)
+        // Identify an action that supports the HTTP POST method. The handler is written using the HTTP protocol, and the method used to upload the files is a POST method.
+        [HttpPost("UploadFiles")]
+        // IFormFileCollection is a collection of the IFormFile object. Which is the equivalent to formData object in vanilla JavaScript.
+        public async Task<IActionResult> UploadFiles(IFormFileCollection files)
         {
-            return View();
+            // Get the physical path to the file's destination folder.
+            var folderPath = Path.Combine(_environment.WebRootPath, "uploads");
+
+            // If the directory doesn't exist, create the directory.
+            if (!Directory.Exists(folderPath))
+            {
+                Directory.CreateDirectory(folderPath);
+            }
+
+            // Iterate through the files in the IFormFileCollection.
+            foreach (var file in files)
+            {
+                // Get the name of each file in the collection.
+                var fileName = Path.GetFileName(file.FileName);
+
+                // Get the physical path to the files destination.
+                var filePath = Path.Combine(folderPath, fileName);
+
+                // Save the files to the server.
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    // The await task is used to allow each file to finish saving before the method continues.
+                    await file.CopyToAsync(fileStream);
+                }
+            }
+
+            // The following code will send the binary data to the external file handler using the HttpClient.
+            using (var client = new HttpClient())
+            {
+                var formData = new MultipartFormDataContent();
+
+                foreach (var file in files)
+                {
+                    formData.Add(new StreamContent(file.OpenReadStream()), "file", file.FileName);
+                }
+
+                var response = await client.PostAsync("http://INSERSERVERHERE.NET/Handler.ashx", formData);
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    return BadRequest("An error has occurred while uploading the files.");
+                }
+            }
+
+            return Ok("The files were successfully uploaded.");
         }
     }
 }
--- web-file-uploader/Models/FileUploadModel.cs
+++ web-file-uploader/Models/FileUploadModel.cs
@@ -5,16 +5,16 @@
         // Add properties needed for the external handler to process the upload properly.
         // Properties needed are:
         // File name
-        public string FileName { get; set; }
+        public string? FileName { get; set; }
         // File type (extension, e.g. .pdf, .txt)
         // TODO: Add validation for accepted file types. Not sure if that's done here, or in the controller, adding this as a reminder.
-        public string FileType { get; set; }
+        public string? FileType { get; set; }
         // File data (which we will store in bytes because the handler accepts the data in bytes, represents the file in binary)
-        public byte[] FileData { get; set; }
+        public byte[]? FileData { get; set; }
         // Date the file was received on.
         // TODO: The user will be able to choose a received on date if the file was received on a prior date.
         // Not sure if this is handled here or in the views. Will update at a later time when necessary. This is a reminder.
-        public DateTime DateReceived { get; set; }
+        public DateTime? DateReceived { get; set; }
         // File size. I chose to go with the int data type because I'm not expecting to have users upload files larger than 2 gigabytes.
         // The long data type is used if you are expecting large files. Declaring the int data type will possibly speed up the application.
         // Need to add validation to cap at 2 gigabytes or the user will receive an error message indicating the file is too large.
